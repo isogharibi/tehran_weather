5b3c5f4ceb0ce92d3a68a011984159dc7a3ff1cc
alireza <abdollah@gmail.com>
2024-06-20 16:58:33 +0330

    add the users and name of the users

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { useRef, useState } from "react";
 
 function App() {
-
   const [UserList, SetUserList] = useState([]);
 
-  return <>
+  fetch(`https://jsonplaceholder.typicode.com/users`)
+    .then((res) => res.json())
+    .then((Users) => SetUserList(Users));
 
-  </>;
+  return (
+      <div>
+       {UserList.map((user) => (
+        <div key={user.id}>
+          <span>name : {user.name}</span>
+        </div>
+       ))}
+      </div>
+  );
 }
 
 export default App;
