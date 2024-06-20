349b239090215c4cc92403a1b83f6c6d2a654818
alireza <abdollah@gmail.com>
2024-06-20 17:57:51 +0330

    push the file to git hub

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from "react";
+import Loding from "./components/Loding";
 
 function App() {
   const [UserList, SetUserList] = useState([]);
@@ -6,18 +7,29 @@ function App() {
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/users`)
       .then((res) => res.json())
-      .then((Users) => SetUserList(Users));
-  },[UserList]);
+      .then((Users) =>
+        setTimeout(() => {
+          SetUserList(Users);
+        }, 3000)
+      );
+  }, [UserList]);
 
   return (
     <div className="flex flex-col gap-x-3 gap-y-4">
-      {UserList.map((user) => (
-        <div className="bg-[#bcfbff] p-4 rounded-md flex flex-col gap-y-1 w-max" key={user.id}>
-          <span>name : {user.name}</span>
-          <span>UserName : {user.username}</span>
-          <span>Email Address : {user.email}</span>
-        </div>
-      ))}
+      {UserList.length ? (
+        UserList.map((user) => (
+          <div
+            className="bg-[#bcfbff] p-4 rounded-md flex flex-col gap-y-1 w-max"
+            key={user.id}
+          >
+            <span>name : {user.name}</span>
+            <span>UserName : {user.username}</span>
+            <span>Email Address : {user.email}</span>
+          </div>
+        ))
+      ) : (
+        <Loding />
+      )}
     </div>
   );
 }
---
+++ src/components/Loding.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Loding() {
+  return (
+    <div className="flex justify-center mt-64 bg-gray-500 ">
+      <img className="Spiner w-10" src="src/assets/icons8-spinner.gif" alt="" />
+    </div>
+  );
+}
+
+export default Loding;
