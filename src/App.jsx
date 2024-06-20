937db0bfb0ca73e48ee15fc3e20f9267e60a43c0
alireza <abdollah@gmail.com>
2024-06-20 17:12:31 +0330

    change the style

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
   const [UserList, SetUserList] = useState([]);
 
-  fetch(`https://jsonplaceholder.typicode.com/users`)
-    .then((res) => res.json())
-    .then((Users) => SetUserList(Users));
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+      .then((res) => res.json())
+      .then((Users) => SetUserList(Users));
+  },[UserList]);
 
   return (
-      <div>
-       {UserList.map((user) => (
-        <div key={user.id}>
+    <div className="flex flex-col gap-x-3 gap-y-4">
+      {UserList.map((user) => (
+        <div className="bg-[#bcfbff] p-4 rounded-md flex flex-col gap-y-1 w-max" key={user.id}>
           <span>name : {user.name}</span>
+          <span>UserName : {user.username}</span>
+          <span>Email Address : {user.email}</span>
         </div>
-       ))}
-      </div>
+      ))}
+    </div>
   );
 }
 
